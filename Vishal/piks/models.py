efc3041f38a5cdb46b3691ca64ff9cf78c7f8986
Vishal Gaud <vgaud6585@gmail.com>
2025-01-29 23:49:21 +0530

    start comits 4

--- Vishal/piks/models.py
+++ Vishal/piks/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class User(models.Model):
    usr_heading = models.CharField(max_length=100)
    usr_description = models.CharField(max_length=100)
-   usr_code_box = models.CharField(max_length=100)
+   usr_code_box = models.CharField(max_length=1000)
    usr_image = models.ImageField(upload_to='images/')
 
    def __str__(self):
