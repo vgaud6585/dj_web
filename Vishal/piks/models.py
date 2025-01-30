855414fa7d78b7aa9c31b9a80fc1b1cbc38611f5
Vishal Gaud <vgaud6585@gmail.com>
2025-01-30 09:16:05 +0530

    start comits 4.2

--- Vishal/piks/models.py
+++ Vishal/piks/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
    usr_heading = models.CharField(max_length=100)
    usr_description = models.CharField(max_length=100)
    usr_code_box = models.CharField(max_length=1000)
-   usr_image = models.ImageField(upload_to='images/')
+   usr_image = models.CharField(max_length=100)
 
    def __str__(self):
        return self.usr_heading
